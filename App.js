6b01676fca266bdfbb23e6272d6b6e8fc5b25505
JulioVianaDev <juliovianadev@gmail.com>
2022-03-16 11:33:06 -0300

    subindo o template

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>bora que é de uva</Text>
       <StatusBar style="auto" />
     </View>
   );
