82ce322dabfd30774d650ed473affa5353349249
JulioVianaDev <juliovianadev@gmail.com>
2022-03-21 15:18:21 -0300

    atualizando state sem o ternário

--- App.js
+++ App.js
@@ -5,24 +5,20 @@ import {
   Text,
   Button,
 } from 'react-native';
-{/* STATE É MUTAVEL E O PROPS É UMA CARACTERISTICA IMUTAVEL*/}
+
 const App = () => {
-  const [name, setName] = useState('Tomate')
-  const [session, setSession] = useState({ number: 6, title: 'The batman' })
-  const [current, setCurrent] = useState(true)
+  const [name, setName] = useState('Me atualize')
 
   const onClickHandler = () => {
-    setName('Otaku')
-    setSession({ number: 7, title: 'Jujust kaisen' })
-    setCurrent(false)
+    setName('Já estou atualizado!')
   }
 
   return (
     <View style={styles.body}>
       <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>Vim ver um filme na sessão {session.number} sobre {session.title}</Text>
-      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
-      <Button title='Update State' onPress={onClickHandler}></Button>
+      <View style={styles.button}>
+        <Button title='Update State' onPress={onClickHandler}></Button>
+      </View>
     </View>
   );
 };
@@ -30,16 +26,24 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#0000ff',
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#ff00ff',
+    borderRadius: 10,
   },
   text: {
-    color: '#ffffff',
-    fontSize: 20,
+    color: '#000000',
+    fontSize: 35,
     fontStyle: 'italic',
     margin: 10,
+    textTransform: 'uppercase'
   },
+  button: {
+    width: 200,
+    height: 60,
+  }
 });
 
 export default App;
