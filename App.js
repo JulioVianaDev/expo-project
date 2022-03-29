8be6dfc99500676348843597e8a5e3697e24b4a1
JulioVianaDev <juliovianadev@gmail.com>
2022-03-29 15:53:37 -0300

    atualizando o template

--- App.js
+++ App.js
@@ -3,122 +3,94 @@ import {
   StyleSheet,
   View,
   Text,
-  ScrollView,
-  RefreshControl,
-  FlatList,
-  SectionList, 
+  TextInput,
+  Button,
   TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
 } from 'react-native';
-import { TextInput } from 'react-native';
-import { Button } from 'react-native';
-import { TouchableHighlight } from 'react-native';
-import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [submitted, setSubtmit] = useState('');
-  
-  const [Refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = () => {
-    setRefreshing(true);
-    setItems([...Items, { name: 'Item 69' }]);
-    setRefreshing(false);
+  const [name, SetName] = useState('');
+  const [submitted, SetSubmitted] = useState(false);
+  const onPressHandler = () => {
+    SetSubmitted(!submitted);
   }
 
-  const onPressHandler =()=>{
-    setSubtmit(!submitted)
-  }
   return (
-
     <View style={styles.body}>
       <Text style={styles.text}>
-        Por favor escreva seu nome
+        Please write your name:
       </Text>
-      <TextInput style={styles.input}
-      multiline
-        placeholder='Ex: Julio Viana'
-        onChangeText={(value)=>setName(value)}
-        maxLength={20}
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. John'
+        onChangeText={(value) => SetName(value)}
       />
-     
-        {/*
-          <Button
-            title={submitted? 'Limpar' : 'Enviar'}
-            onPress={onPressHandler}
-            // este argumento serve para desativar o botão depois dele ser ativado
-            //disabled={submitted}
-            color={'#ff00ff'}
-          />
-        */}
-      {/* <TouchableHighlight
+      {/* <Button
+        title={submitted ? 'Clear' : 'Submit'}
         onPress={onPressHandler}
-        style={styles.button}
-        activeOpacity={0.2}
+        color='#00f'
+      /> */}
+      {/* <TouchableWithoutFeedback
+        onPress={onPressHandler}
       >
-        <Text
-          style={styles.text}
-        >   
-          {submitted? 'Limpar' : 'Enviar'}
-        </Text>
-      </TouchableHighlight> */}
+        <View style={styles.button}>
+          <Text style={styles.text}>
+            {submitted ? 'Clear' : 'Submit'}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback> */}
       <Pressable
-        style={({pressed})=>[
-          {backgroundColor: pressed ? '#dddddd' : '#00ff00' },
+        onPress={onPressHandler}
+        hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
+        android_ripple={{color:'#00f'}}
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? '#dddddd' : '#00ff00' },
           styles.button
         ]}
-        onLongPress={onPressHandler}
-        delayLongPress={2000}
-        onPress={onPressHandler}
-        hitSlop={{top: 10, bottom: 10, right: 10, left: 10}}
-
       >
-        {submitted? 'Clear': 'Enviar'}
-      </Pressable>
-      {submitted? 
         <Text style={styles.text}>
-          Você foi registrado com {name} 
+          {submitted ? 'Limpar' : 'Enviar'}
+        </Text>
+      </Pressable>
+      {submitted ?
+        <Text style={styles.text}>
+          You are registered as {name}
         </Text>
         :
         null
-    }
-       
+      }
     </View>
-   
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#ffffff',
-    alignItems:'center',
-  },
-  input:{
-    width: 200,
-    borderWidth: 1,
-    borderColor:'#ff00ff',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 14,
-  },
-  button:{
-    marginBottom: 10,
-    borderColor:'#ff00ff',
-    backgroundColor: '#4ae1fa',
-  },
-  item: {
-    margin: 10,
-    backgroundColor: '#4ae1fa',
-    justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
     color: '#000000',
-    fontSize: 45,
-    fontStyle: 'italic',
+    fontSize: 20,
     margin: 10,
+  },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  button: {
+    width: 150,
+    height: 50,
+    alignItems: 'center',
   },
 });
 
